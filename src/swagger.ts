9b4d1c2a77eb0f380e1c48e78968e1143b6cd3ab
Ismoil Aburahmonov <ismoil4432@gmail.com>
2023-09-26 14:09:29 +0500

    updated, template to start

--- src/swagger.ts
+++ src/swagger.ts
@@ -10,23 +10,23 @@ export const swaggerConfig = new DocumentBuilder()
     },
     'admin_auth',
   )
-  .setTitle('API doc for ResPos')
+  .setTitle('API doc for Nest Gym')
   .setDescription("Qo'ldan kelgancha qilindi;)")
   .setVersion('1.0.0')
   .build();
 
 export const swaggerOptions: SwaggerCustomOptions = {
-  customSiteTitle: 'ResPos Api',
-  customfavIcon: 'https://pos.in1.uz/api/static/invan.png',
+  customSiteTitle: 'Nest Gym Api',
+  customfavIcon: 'https://www.svgrepo.com/show/195284/weights-gym.svg',
   customCss: `
         .topbar {
             background-color: #315881 !important;
             padding: 20px !important;
         }
-        .topbar-wrapper img {
-          content:url(https://pos.in1.uz/api/static/invan_swagger_logo.png);
-          width:auto;
-          height:auto;
+        .topbar-wrapper {
+          content:url(https://www.svgrepo.com/show/195284/weights-gym.svg);
+          width:100px;
+          height:50px;
         }
         .swagger-ui .topbar { 
           border-bottom: 5px solid #5dc6d1;
